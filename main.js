95065b283bb11fe6a613a3cead0cf3042304d940
ramazanyamac <rramazanyamac@gmail.com>
2024-02-16 18:02:12 +0300

    basic js example added

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
-import './main.scss';
-import $ from 'jquery';
+import "./main.scss";
+import axios from "axios";
 
-$(document).ready(function() {
-  console.log('jQuery working!');
+function cardComponent(image, title) {
+    const card = `
+    <div class="card">
+        <a href="#">
+          <img src="${image}" alt="${title}" class="card__img" loading="lazy">
+        </a>
+        <div class="card__body">
+          <h5 class="card__title">${title}</h5>
+          <time datetime="2020-01-01" class="card__time">Jul 1, 2020</time>
+        </div>
+    </div>`;
+
+    return card;
+}
+
+async function getCat() {
+    try {
+        const res = await axios.get("https://jsonplaceholder.typicode.com/albums/1/photos");
+        return res;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", async function () {
+    const { data } = await getCat();
+    const cardContainer = document.querySelector(".js-card-container");
+    data.forEach((item) => {
+        cardContainer.innerHTML += cardComponent(item.url, item.title)
+    });
 });
